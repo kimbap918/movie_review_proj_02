af53f5892204ca50ca36a989c1373ad4937092cd
최준혁 <fx887722@naver.com>
2022-10-27 19:39:05 +0900

    댓글 작성, 댓글 목록 출력 완료

--- reviews/urls.py
+++ reviews/urls.py
@@ -9,4 +9,6 @@ urlpatterns = [
     path('community/', views.community, name='community'),
     path('detail/<int:pk>', views.detail, name='detail'),
     path('update/<int:pk>', views.update, name='update'),
+    path('delete/<int:pk>', views.delete, name='delete'),
+    path('comments/<int:pk>', views.comment_create, name='comment_create'),
 ]
